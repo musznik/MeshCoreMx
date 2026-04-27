5b4a30aab7f67e44b7f9a50c0df289d8c88eb509
Quency-D <55523105+Quency-D@users.noreply.github.com>
2026-04-27 14:06:21 +0800

    Change write to read for radio_fem_rxgain

--- examples/companion_radio/DataStore.cpp
+++ examples/companion_radio/DataStore.cpp
@@ -234,6 +234,7 @@ void DataStore::loadPrefsInt(const char *filename, NodePrefs& _prefs, double& no
     file.read((uint8_t *)&_prefs.radio_fem_rxgain, sizeof(_prefs.radio_fem_rxgain)); // 90
     file.read((uint8_t *)_prefs.default_scope_name, sizeof(_prefs.default_scope_name));    // 90
     file.read((uint8_t *)_prefs.default_scope_key, sizeof(_prefs.default_scope_key));     // 121
+    file.read((uint8_t *)&_prefs.radio_fem_rxgain, sizeof(_prefs.radio_fem_rxgain));      // 122
 
     file.close();
   }
